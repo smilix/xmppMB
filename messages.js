4eb8043ddbc89b4d1305194941e58549fa7c2bdf
holger <holgercremer@gmail.com>
2012-03-18 17:36:07 +0100

    messages for the ips version

---
+++ messages.js
@@ -0,0 +1,24 @@
+/**
+ * all public messages stored here.
+ */
+
+module.exports = {
+  /* index.html */
+  title : 'ips-note',
+  statusOk : 'Verbindung offen, alles OK.',
+  statusError : 'Verbindung unterbrochen!',
+  close : 'Schließen',
+  ankerPost1 : 'P',
+  ankerPost2 : 'ost',
+  messagePlaceholder : 'Deine Nachricht ans Team',
+  cancel : 'Abbrechen',
+  send : 'Senden',
+  /* app.js */
+  errorGeneric : 'Internal server error.',
+  errorUserNotAllowed : 'Du darfst nicht senden.',
+  /* RssBuilder.js */
+  feedDescription : 'Alle Nachrichten aus dem Jabber Gruppenchat \'%s\'.',
+  feedTitle: 'ips-note',
+  feedItemTitle: 'Von: %s'
+    
+};
